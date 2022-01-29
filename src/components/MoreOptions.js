1dfdf35a5b0bc62223d53433bc27dfa8364932a9
zacwalker93 <zacwalker93@gmail.com>
2022-01-28 21:29:24 -0500

    fix menu

--- src/components/MoreOptions.js
+++ src/components/MoreOptions.js
@@ -1,52 +1,86 @@
-import React from "react";
+import * as React from 'react';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Stack from '@mui/material/Stack';
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import IconButton from "@material-ui/core/IconButton";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-  
-const MoreOptions = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  
-  const options = [
-    "Save",
-    "Hide",
-    "Delete",
-  ];
-  
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+import { IconButton } from '@mui/material';
+
+
+export default function MoreOptions() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
   };
-  
-  const open = Boolean(anchorEl);
-  
-  const handleClose = () => {
-    setAnchorEl(null);
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
-  
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
   return (
-    <div>
-      <IconButton
-        aria-label="more"
-        onClick={handleClick}
-        aria-haspopup="true"
-        aria-controls="long-menu"
-      >
-        <MoreVertIcon />
-      </IconButton>
-      <Menu 
-        anchorEl={anchorEl} 
-        keepMounted onClose={handleClose} 
-        open={open}>
-        {options.map((option) => (
-          <MenuItem
-            key={option} 
-            onClick={handleClose}>
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
-    </div>
+      <div>
+        <IconButton
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? 'composition-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <MoreVertIcon />
+
+        </IconButton>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
   );
-};
-  
-export default MoreOptions;
+}
